16e7cb46a28b90536225346e6cc9758761cf0816
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-29 19:31:45 +0900

    feat :: header

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -3,21 +3,31 @@ import styled from '@emotion/styled';
 export const Container = styled.header`
   display: flex;
   align-items: center;
+  width: 100%;
   padding: 15px;
-  background-color: pink;
+  background-color: yellowgreen;
+  margin: 0;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
 `
 
 export const Logo = styled.img`
-  width: 100px;
+  width: 110px;
   height: auto;
+  margin-left: 4rem;
+  align-items: center;
 `
 
 export const Nav = styled.nav`
-    margin-left: auto;
+  margin-left: 1rem;
+  margin-top: 0.4rem;
 `
 
 export const NavItem = styled.a`
-    color: #333;
-    text-decoration: none;
-    cursor: pointer;
+  color: #333;
+  cursor: pointer;
+  font-size: 0.8rem;
+  padding: 0.7rem;
 `
