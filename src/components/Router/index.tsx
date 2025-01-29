57b146c00271c8d8f9d511ae33e89904d1916e3b
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-29 12:41:46 +0900

    fix :: 불필요한 공백 삭제

--- src/components/Router/index.tsx
+++ src/components/Router/index.tsx
@@ -1,13 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from '../../pages/Home';
-import Login from '../../pages/Login";
 
 const Router = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element = {<Home />} />
-                <Route path="/login" element = {<Login />} />
+                <Route path="/" element = {<Home />}/>
             </Routes>
         </BrowserRouter>
     )
