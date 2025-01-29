0397a3a984f64819a072fbc11224d6f053385f43
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-29 16:38:48 +0900

    feat :: NavItem

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -5,9 +5,23 @@ export const Container = styled.header`
   align-items: center;
   padding: 10px;
   background-color: pink;
-`;
+`
 
 export const Logo = styled.img`
   width: 100px;
   height: auto;
-`;
+`
+
+export const Nav = styled.nav`
+    margin-left: auto;
+`
+
+export const NavItem = styled.a`
+    margin-left: 10px;
+    color: #333;
+    text-decoration: none;
+    cursor: pointer;
+    &:hover {
+        color: #f00;
+    }
+`
