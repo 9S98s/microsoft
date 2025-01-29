8f5313deb7127586369f24f9f295a2adf2fe71ae
윤효진 <hyojinee@dgsw.hs.kr>
2025-01-29 15:26:59 +0900

    feat :: 푸터 렌더링

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,1 +1,9 @@
 import styled from "@emotion/styled";
+
+export const Container = styled.footer`
+    background-color: #333;
+    color: #fff;
+    text-align: center;
+    padding: 1rem;
+    margin-top: 1rem;
+`
